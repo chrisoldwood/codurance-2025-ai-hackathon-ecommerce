fcb4fe15528a6247f5209f26f68d7be5cd06c69f
Chris Oldwood <gort@cix.co.uk>
2025-04-26 13:05:31 +0100

    Second test -- page transition.

--- StyleDenTests/ProductPageTests.cs
+++ StyleDenTests/ProductPageTests.cs
@@ -1,8 +1,9 @@
 ﻿namespace StyleDenTests;
 
 using System.Collections.Generic;
-
+using AngleSharp.Html.Dom.Events;
 using Bunit;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using StyleDen.Components.Pages;
 
@@ -22,13 +23,28 @@ public class ProductPageTests : TestContext
     [Fact]
     public void Products_page_contains_list_of_products()
     {
-
         Services.AddSingleton<IProductService>(new fakeProductService());
 
-        var productPageComponent = RenderComponent<Home>();
+        var productsPageComponent = RenderComponent<Home>();
         
-        var productsTable = productPageComponent.Find("#products-table");
+        var productsTable = productsPageComponent.Find("#products-table");
 
         Assert.Contains("Shirt", productsTable.InnerHtml);
+    }
+
+    [Fact]
+    public void Clicking_a_product_navigates_to_the_product_details_page()
+    {
+        Services.AddSingleton<IProductService>(new fakeProductService());
+
+        var visiblePageComponent = RenderComponent<Home>();
+
+        var product = visiblePageComponent.Find("#product-2");
+        Assert.NotNull(product);
+
+        product.ClickAsync(new MouseEventArgs());
+        var productDetailsPage = visiblePageComponent.FindComponent<ProductDetails>();
+
+        Assert.NotNull(productDetailsPage);
     }
 }
